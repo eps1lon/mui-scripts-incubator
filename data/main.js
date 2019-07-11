39e8c13cc316bf2431318a1dfa4f453fd76b3efc
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-07-11 11:05:49 +0000

    switch to pipeline

--- data/main.js
+++ data/main.js
@@ -1,8 +1,12 @@
+const stream = require("stream");
+const util = require("util");
 const usedBy = require("./streams/usedBy");
 const filterInteresting = require("./streams/filterInteresting");
 const downloadRepo = require("./streams/downloadRepo");
 const filterUsageFiles = require("./streams/filterUsageFiles");
 const filterUsageCode = require("./streams/filterUsageCode");
+
+const pipeline = util.promisify(stream.pipeline);
 
 main().catch(err => {
   console.error(err);
@@ -10,44 +14,31 @@ main().catch(err => {
 });
 
 async function main() {
-  usedBy("mui-org", "material-ui")
-    .pipe(
-      filterInteresting(repository => repository.stars >= 0).on(
-        "data",
-        repository =>
-          console.log(
-            `interesting repository ${repository.orgName}/${
-              repository.repoName
-            }`
-          )
-      )
-    )
-    .pipe(
-      downloadRepo().on("data", ({ entry, repository }) => {
+  await pipeline(
+    usedBy("mui-org", "material-ui"),
+    filterInteresting(repository => repository.stars >= 0).on(
+      "data",
+      repository =>
         console.log(
-          `downloaded ${entry.path} in ${repository.orgName}/${
-            repository.repoName
-          }`
-        );
-      })
-    )
-    .pipe(
-      filterUsageFiles().on("data", file => {
-        console.log(
-          `file with mui usage: ${file.name} in ${file.repository.orgName}/${
-            file.repository.repoName
-          }`
-        );
-      })
-    )
+          `interesting repository ${repository.orgName}/${repository.repoName}`
+        )
+    ),
+    downloadRepo().on("data", ({ entry, repository }) => {
+      console.log(
+        `downloaded ${entry.path} in ${repository.orgName}/${
+          repository.repoName
+        }`
+      );
+    }),
+    filterUsageFiles().on("data", file => {
+      console.log(
+        `file with mui usage: ${file.name} in ${file.repository.orgName}/${
+          file.repository.repoName
+        }`
+      );
+    }),
+    filterUsageCode(),
+    new stream.PassThrough({objectMode: tru})
+  );
 
-    .pipe(filterUsageCode())
-    .on("data", createMonitor("filterUsageCode"));
-}
-
-function createMonitor(name) {
-  return function monitor(data) {
-    console.log(`received data on ${name}: ${data.orgName}/${data.repoName}`);
-    //console.log(data);
-  };
 }
