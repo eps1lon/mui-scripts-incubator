ce34457f1c10887857a1464c15d5382d6df9f212
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-03-07 16:25:34 +0100

    fix: module not found

--- actions/watch-material-ui/main.js
+++ actions/watch-material-ui/main.js
@@ -1,6 +1,6 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const a11ySnapshot = require("../lib/a11y-snapshot");
+const a11ySnapshot = require("../../lib/a11y-snapshot");
 
 main().catch(error => {
   core.setFailed(error.message);
