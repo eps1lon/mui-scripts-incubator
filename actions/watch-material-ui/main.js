efed4d9e55ebff8ce6a8fe1fa93df698e497d083
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-03-07 16:10:56 +0100

    first iteration

---
+++ actions/watch-material-ui/main.js
@@ -0,0 +1,17 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const a11ySnapshot = require("../lib/a11y-snapshot");
+
+main().catch(error => {
+  core.setFailed(error.message);
+});
+
+async function main() {
+  const { eventName, event } = github.context;
+
+  if (eventName === "push") {
+    a11ySnapshot({ argv: ["--update"] });
+  } else if (eventName === "repository_dispatch") {
+    a11ySnapshot({ prNumber: event.client_payload.pr_number });
+  }
+}
