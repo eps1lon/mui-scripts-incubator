14573e8e6cbbe929ed5b6881a8af548c006d911e
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-04-17 13:38:59 +0200

    fix: remove flak in-house ad

--- lib/a11y-snapshot/run.sh
+++ lib/a11y-snapshot/run.sh
@@ -4,7 +4,7 @@
 #set -e
 for (( ; ; ))
 do
-  yarn a11y-snapshot --testNamePattern="firefox /"
+  yarn a11y-snapshot --testNamePattern="firefox \/components"
   exitCode=$?
   if [ $exitCode -ne 0 ]; then
     exit $exitCode
