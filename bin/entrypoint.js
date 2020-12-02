da2e4ec3af73e96fda4632d64bd7e17f62561bae
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-12-02 19:26:49 +0100

    Check deploy meta data for a11y-snapshot into vcs (#1454)

--- bin/entrypoint.js
+++ bin/entrypoint.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 const lighthouseAudit = require("../lib/lighthouse-audit");
-const a11ySnapshot = require("../lib/a11y-snapshot");
 
 require("yargs")
 	.command({
@@ -15,21 +14,6 @@ require("yargs")
 		},
 		handler: (argv) => {
 			return lighthouseAudit({ prNumber: +argv["pr-number"] });
-		},
-	})
-	.command({
-		command: "a11y-snapshot [pr-number]",
-		desc: "checks a11y tree snapshots",
-		builder: (yargs) => {
-			yargs.positional("pr-number", {
-				describe:
-					"number of a Pull Request or empty to target the default branch",
-				type: "number",
-				default: Number.NaN,
-			});
-		},
-		handler: (argv) => {
-			a11ySnapshot({ prNumber: +argv["pr-number"] });
 		},
 	})
 	.help().argv;
