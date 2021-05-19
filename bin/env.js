b1af7741a2b8490236ee1edf5b4bd0e09eb7da76
GitHub Action <action@github.com>
2021-05-19 09:46:52 +0200

    debug yarn spawn

--- bin/env.js
+++ bin/env.js
@@ -1,3 +1,4 @@
+const childProcess = require("child_process");
 const playwright = require("playwright");
 const os = require("os");
 
@@ -12,9 +13,15 @@ async function main() {
 			return [name, version];
 		})
 	);
+	const yarnVersion = await new Promise((resolve, reject) => {
+		const yarnProcess = childProcess.spawnSync("yarn", ["--version"]);
+		console.log(yarnProcess);
+		resolve(yarnProcess.stdout);
+	});
 
 	console.log("Node\t%s", process.version);
 	console.log("OS\t%s, %s, %s, %s", os.platform, os.type, os.version, os.arch);
+	console.log("yarn\t%s", yarnVersion);
 	console.log("Browsers:");
 	browsers.map(([name, version]) => {
 		console.log("  %s\t%s", name, version);
