8cca4ebcdcd58e98e7a8a16fc69ca7a9eee5b1d4
GitHub Action <action@github.com>
2021-05-19 09:57:38 +0200

    fix: Use jest binary directly

--- bin/env.js
+++ bin/env.js
@@ -1,4 +1,3 @@
-const childProcess = require("child_process");
 const playwright = require("playwright");
 const os = require("os");
 
@@ -13,19 +12,9 @@ async function main() {
 			return [name, version];
 		})
 	);
-	const yarnVersion = await new Promise((resolve, reject) => {
-		const yarnProcess = childProcess.spawnSync("yarn", ["--version"]);
-		console.log(yarnProcess);
-		if (yarnProcess.stderr) {
-			reject(yarnProcess.stderr);
-		}
-		resolve(yarnProcess.stdout);
-	});
 
-	console.log(process.env);
 	console.log("Node\t%s", process.version);
 	console.log("OS\t%s, %s, %s, %s", os.platform, os.type, os.version, os.arch);
-	console.log("yarn\t%s", yarnVersion);
 	console.log("Browsers:");
 	browsers.map(([name, version]) => {
 		console.log("  %s\t%s", name, version);
