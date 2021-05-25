fb4c2587558fc6df32e35c3db9215452aafbf63e
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2021-05-25 10:21:39 +0200

    test(screen-reader): Add current behavior for time picker keyboard navigation (#2416)

--- lib/a11y-snapshot/screen-reader-tests.ps1
+++ lib/a11y-snapshot/screen-reader-tests.ps1
@@ -1,7 +1,5 @@
-# The path you cloned this repository to
-$REPO_PATH = "C:\Users\eps1lon\Development\mui-scripts-incubator"
-
 # End of configuration. Changing anything below at your own risk
+$REPO_PATH = (Get-Location).Path
 $LOG_FILE_PATH = "$REPO_PATH\lib\a11y-snapshot\nvda.log"
 $NVDA_BIN = "$REPO_PATH\node_modules\screen-reader-testing-library\bin\nvda.ps1"
 & $NVDA_BIN -logFile $LOG_FILE_PATH
